8919e8ba2c39370b33d5a58276511b162fbaaa43
Cyrill Bader <cyrill.bader@akros.ch>
2020-05-19 10:18:43 +0200

    Fixed Budgeted Value depending on current Date

--- src/main/java/ch/bader/budget/server/calculation/implementation/function/BudgetedAmountFunction.java
+++ src/main/java/ch/bader/budget/server/calculation/implementation/function/BudgetedAmountFunction.java
@@ -1,5 +1,6 @@
 package ch.bader.budget.server.calculation.implementation.function;
 
+import java.time.LocalDate;
 import java.util.function.Function;
 
 import ch.bader.budget.server.entity.Transaction;
@@ -8,6 +9,10 @@ public class BudgetedAmountFunction implements Function<Transaction, Number> {
 
 	@Override
 	public Number apply(Transaction t) {
+		LocalDate firstDayOfThisMonth = LocalDate.now().withDayOfMonth(1);
+		if (firstDayOfThisMonth.isAfter(t.getDate())) {
+			return t.getEffectiveAmount();
+		}
 		return t.getBudgetedAmount();
 	}
 
