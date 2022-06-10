19f3f52765ce62ef7a8bba6bdee1ba1ff43b4949
cyrill10 <cyrill.bader@gmx.ch>
2022-06-10 21:28:56 +0200

    mongofy realaccount

--- src/test/java/ch/bader/budget/server/mapper/RealAccountMapperTest.java
+++ src/test/java/ch/bader/budget/server/mapper/RealAccountMapperTest.java
@@ -1,8 +1,10 @@
 package ch.bader.budget.server.mapper;
 
 import ch.bader.budget.server.adapter.mongo.entity.RealAccountDbo;
+import ch.bader.budget.server.adapter.mongo.entity.ValueEnumDbo;
 import ch.bader.budget.server.adapter.sql.entity.RealAccountDboSql;
 import ch.bader.budget.server.boundary.dto.RealAccountBoundaryDto;
+import ch.bader.budget.server.boundary.dto.ValueEnumDto;
 import ch.bader.budget.server.domain.RealAccount;
 import ch.bader.budget.server.type.AccountType;
 import org.junit.jupiter.api.Test;
@@ -37,6 +39,28 @@ class RealAccountMapperTest {
     }
 
     @Test
+    public void shouldMapDtoToRealAccount() {
+        //given
+        RealAccountBoundaryDto dto = RealAccountBoundaryDto.builder()
+                                                           .id("id")
+                                                           .name("realAccountName")
+                                                           .accountType(ValueEnumDto.builder()
+                                                                                    .value(AccountType.CHECKING.getValue())
+                                                                                    .name(AccountType.CHECKING.getName())
+                                                                                    .build())
+                                                           .build();
+
+        //when
+        RealAccount account = sut.mapToDomain(dto);
+
+        //then
+        assertThat(account).isNotNull();
+        assertThat(account.getName()).isEqualTo("realAccountName");
+        assertThat(account.getId()).isEqualTo("id");
+        assertThat(account.getAccountType()).isEqualTo(AccountType.CHECKING);
+    }
+
+    @Test
     public void shouldMapRealAccountToDbo() {
         //given
         RealAccount domain = RealAccount.builder()
@@ -56,6 +80,28 @@ class RealAccountMapperTest {
     }
 
     @Test
+    public void shouldMapDboToRealAccount() {
+        //given
+        RealAccountDbo dbo = RealAccountDbo.builder()
+                                           .id("id")
+                                           .name("realAccountName")
+                                           .accountType(ValueEnumDbo.builder()
+                                                                    .value(AccountType.CHECKING.getValue())
+                                                                    .name(AccountType.CHECKING.getName())
+                                                                    .build())
+                                           .build();
+
+        //when
+        RealAccount account = sut.mapToDomain(dbo);
+
+        //then
+        assertThat(account).isNotNull();
+        assertThat(account.getName()).isEqualTo("realAccountName");
+        assertThat(account.getId()).isEqualTo("id");
+        assertThat(account.getAccountType()).isEqualTo(AccountType.CHECKING);
+    }
+
+    @Test
     public void shouldMapRealAccountToOldDbo() {
         //given
         RealAccount domain = RealAccount.builder()
@@ -69,6 +115,25 @@ class RealAccountMapperTest {
 
         //then
         assertThat(account).isNotNull();
+        assertThat(account.getId()).isEqualTo(1);
+        assertThat(account.getName()).isEqualTo("realAccountName");
+        assertThat(account.getAccountType()).isEqualTo(AccountType.CHECKING);
+    }
+
+    @Test
+    public void shouldMapOldDboToRealAccount() {
+        //given
+        RealAccountDboSql dbo = new RealAccountDboSql();
+        dbo.setId(1);
+        dbo.setName("realAccountName");
+        dbo.setAccountType(AccountType.CHECKING);
+
+        //when
+        RealAccount account = sut.mapToDomain(dbo);
+
+        //then
+        assertThat(account).isNotNull();
+        assertThat(account.getId()).isEqualTo("1");
         assertThat(account.getName()).isEqualTo("realAccountName");
         assertThat(account.getAccountType()).isEqualTo(AccountType.CHECKING);
     }
