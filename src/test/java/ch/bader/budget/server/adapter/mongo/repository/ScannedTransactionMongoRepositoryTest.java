a010181482e9cba4fc441c641b6fdeb243bc8dd3
cyrill10 <cyrill.bader@gmx.ch>
2022-09-10 17:51:45 +0200

    fix disabled test

--- src/test/java/ch/bader/budget/server/adapter/mongo/repository/ScannedTransactionMongoRepositoryTest.java
+++ src/test/java/ch/bader/budget/server/adapter/mongo/repository/ScannedTransactionMongoRepositoryTest.java
@@ -1,13 +1,14 @@
 package ch.bader.budget.server.adapter.mongo.repository;
 
 import ch.bader.budget.server.adapter.mongo.entity.ScannedTransactionDbo;
+import ch.bader.budget.server.converter.Converters;
 import ch.bader.budget.server.converter.DataMongoTypeExcludeFilterWithConverter;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.autoconfigure.filter.TypeExcludeFilters;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
 
 import java.time.YearMonth;
@@ -18,13 +19,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataMongoTest
 @OverrideAutoConfiguration(enabled = true)
 @TypeExcludeFilters(DataMongoTypeExcludeFilterWithConverter.class)
+@Import(Converters.class)
 class ScannedTransactionMongoRepositoryTest {
 
     @Autowired
     ScannedTransactionMongoRepository sut;
 
     @Test
-    @Disabled
     @DirtiesContext
     void findAllByYearMonth() {
 
